23a8e69b09bff5b8ae85dfdb6109f255598c51e9
Pazhiloi <martinsen06@gmail.com>
2021-02-18 16:38:55 +0200

    added index page html and css

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,19 @@ $(function () {
     autoplay: true,
     autoplaySpead: 200
   });
+
+
+
+
+    var mixer = mixitup('.product__items', {
+     selectors: {
+       control: '.product__btn'
+     }
+   });
+    var mixer1 = mixitup('.design__items', {
+     selectors: {
+       control: '.design__btn'
+     }
+   });
+
 });
